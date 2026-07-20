7cbf4421f37907159cddf98f25e30bcce524c7bc
MarcSpark <lemakautomation@gmail.com>
2026-07-20 07:58:44 +0200

    Mission 016 - Enterprise logging framework implemented
    Created centralized Logger class and updated server startup to use structured logging.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import app from "./app";
 import { applicationConfig } from "./config/application.config";
 import { serverConfig } from "./config/server.config";
+import { logger } from "./logging/logger";
 
 app.listen(serverConfig.port, () => {
-    console.log(
+    logger.info(
         `${applicationConfig.name} v${applicationConfig.version} running on ${serverConfig.host}:${serverConfig.port}`
     );
 });
